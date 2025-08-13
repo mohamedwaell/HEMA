f73aa76634de47a3454f47b7b971db99b7a906e8
Eng-HEMA <hshaabrahym36@gmail.com>
2025-08-13 06:15:46 +0300

    my be this is the end

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,55 +1,55 @@
-// import { useGSAP } from "@gsap/react";
-// import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import TitleHeader from "../components/TitleHeader";
-// import TechIconCardExperience from "../components/models/tech_logos/TechIconCardExperience";
-import {  techStackImgs } from "../constants";
-// import { useEffect, useState } from "react";
+import TechIconCardExperience from "../components/models/tech_logos/TechIconCardExperience";
+import {  techStackImgs,techStackIcons } from "../constants";
+import { useEffect, useState } from "react";
 
 const TechStack = () => {
-  // const [isMobile, setIsMobile] = useState(true);
+  const [isMobile, setIsMobile] = useState(true);
 
-  // useGSAP(() => {
-  //   gsap.fromTo(
-  //     ".tech-card",
-  //     { y: 100, opacity: 0 },
-  //     {
-  //       y: 0,
-  //       opacity: 1,
-  //       duration: 1,
-  //     ease: "power3.out",
-  //       scrollTrigger: {
-  //         trigger: "#skills",
-  //         start: "top 80%", 
-  //         end: "top 30%",   
-  //         scrub: 1,         
-  //       },
-  //     }
-  //   );
-  //   gsap.fromTo(
-  //     ".flow",
-  //     {
-  //       y:-20,
+  useGSAP(() => {
+    // gsap.fromTo(
+    //   ".tech-card",
+    //   { y: 100, opacity: 0 },
+    //   {
+    //     y: 0,
+    //     opacity: 1,
+    //     duration: 1,
+    //   ease: "power3.out",
+    //     scrollTrigger: {
+    //       trigger: "#skills",
+    //       start: "top 80%", 
+    //       end: "top 30%",   
+    //       scrub: 1,         
+    //     },
+    //   }
+    // );
+    gsap.fromTo(
+      ".flow",
+      {
+        y:-20,
          
 
-  //     },{
-  //       y:20,
-  //       duration:2,
-  //       ease:"power1.inOut",
-  //       repeat:-1,
-  //       yoyo:true,
-  //     }
-  //   )
-  // });
+      },{
+        y:20,
+        duration:2,
+        ease:"power1.inOut",
+        repeat:-1,
+        yoyo:true,
+      }
+    )
+  });
 
-  // useEffect(() => {
-  //   const updateControls = () => {
-  //     setIsMobile(window.innerWidth < 640);
-  //   };
+  useEffect(() => {
+    const updateControls = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
 
-  //   updateControls();
-  //   window.addEventListener("resize", updateControls);
-  //   return () => window.removeEventListener("resize", updateControls);
-  // }, []);
+    updateControls();
+    window.addEventListener("resize", updateControls);
+    return () => window.removeEventListener("resize", updateControls);
+  }, []);
 
   return (
     <div id="skills" className="flex-center section-padding">
@@ -60,17 +60,17 @@ const TechStack = () => {
         />
         <div className="tech-grid">
           {
-          // isMobile
-          //   ? 
+          isMobile
+            ? 
             techStackImgs.map((techStackIcon, index) => (
                 <div
                   key={index}
                   className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
                 >
-                  <div className="tech-card-animated-bg" />
+                  <div className="" />
                   <div className="tech-card-content">
                     <div className="tech-icon-wrapper">
-                      <img src={techStackIcon.imgPath} alt="" className="w-32 cardan" />
+                      <img src={techStackIcon.imgPath} alt="" className="w-32 flow" />
                     </div>
                     <div className="padding-x w-full">
                       <p>{techStackIcon.name}</p>
@@ -78,22 +78,22 @@ const TechStack = () => {
                   </div>
                 </div>
               ))
-            // : techStackIcons.map((techStackIcon) => (
-            //     <div
-            //       key={techStackIcon.name}
-            //       className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
-            //     >
-            //       <div className="tech-card-animated-bg" />
-            //       <div className="tech-card-content">
-            //         <div className="tech-icon-wrapper">
-            //           <TechIconCardExperience model={techStackIcon} />
-            //         </div>
-            //         <div className="padding-x w-full">
-            //           <p>{techStackIcon.name}</p>
-            //         </div>
-            //       </div>
-            //     </div>
-            //   ))
+            : techStackIcons.map((techStackIcon) => (
+                <div
+                  key={techStackIcon.name}
+                  className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+                >
+                  <div className="" />
+                  <div className="tech-card-content">
+                    <div className="tech-icon-wrapper">
+                      <TechIconCardExperience model={techStackIcon} />
+                    </div>
+                    <div className="padding-x w-full">
+                      <p>{techStackIcon.name}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
               }
         </div>
       </div>
